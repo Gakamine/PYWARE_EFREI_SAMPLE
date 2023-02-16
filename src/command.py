49ab885fd77080730bf7be9f5f08103f23cfb3dd
Gakamine <jolyantoine@pm.me>
2023-02-16 23:17:20 +0100

    Fix Windows CMD

--- src/command.py
+++ src/command.py
@@ -1,6 +1,6 @@
 import subprocess
 
 def execute_cmd(cmd):
-    output=subprocess.run(cmd, stdout=subprocess.PIPE).stdout.decode("utf-8")
+    output=subprocess.run(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True, text=True).stdout
     output=output.replace('\r', '').replace('\n', '')
     return output
